f74b5590d778e5babbfa01c90622101f7ecf85f9
omar208 <55194832+omar208@users.noreply.github.com>
2021-11-14 20:40:12 +0200

    notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,37 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:socialapp/pages/mainchat/chat.dart';
 import 'package:socialapp/pages/signin/login.dart';
 import 'package:socialapp/shared/cache.dart';
 import 'package:socialapp/shared/data.dart';
+import 'package:socialapp/shared/sharedwadget.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  var token = await FirebaseMessaging.instance.getToken();
+
+  print(token);
+
+  // foreground fcm
+  FirebaseMessaging.onMessage.listen((event) {
+    print('on message');
+    print(event.data.toString());
+
+    toast(txt: 'on massage');
+  });
+
+  // when click on notification to open app
+  FirebaseMessaging.onMessageOpenedApp.listen((event) {
+    print('on message opened app');
+    print(event.data.toString());
+    toast(txt: 'on massage');
+  });
+
+  // background fcm
+  // FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+
   await CacheHelper.init();
   uId = CacheHelper.getData(key: 'UserUid');
   runApp(MyApp(uId));
